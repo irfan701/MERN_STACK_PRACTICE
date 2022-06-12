0d4bc13ef76f5d96e8fef215e02f4b7c10f9caf3
irfan701 <irfanhossain701@gmail.com>
2022-06-12 17:54:56 +0400

    Response status
    Response status

--- day1/index.js
+++ day1/index.js
@@ -6,6 +6,39 @@ app.get("/",(req,res)=>{
     res.send("Hello Express Js");
 })
 
+app.post("/home",(req,res)=>{
+    res.send('Home Page')
+})
+
+app.put("/about",(req,res)=>{
+    res.send('About Page')
+})
+
+app.delete("/contact",(req,res)=>{
+    res.send('Contact Page')
+})
+
+//simple string  response
+
+//res.send() ->denoted to body
+//res.end()  ->response ending point
+app.get("/one",(req,res)=>{
+    res.send("This is simple string response!");
+})
+
+app.post("/two",(req,res)=>{
+    res.end("This is simple string response")
+})
+
+//Response Status code
+app.post("/three",(req,res)=>{
+    res.status('401').end('unauthorized...');
+
+})
+app.post('/four',(req,res)=>{
+    res.status("201").end('Created...');
+})
+
 
 app.listen(4000,()=>{
     console.log(`Server Run Success ${4000}`)
